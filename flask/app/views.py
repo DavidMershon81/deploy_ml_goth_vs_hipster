441bd6f9bb12d754b2d32b2826a4b11ee5e785d8
= <dmershon@universalhappymaker.com>
2020-12-15 09:06:49 -0800

    started adding a page to upload images

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,8 +1,19 @@
 from app import app
 from app import test_model_load
 from flask import render_template
+from flask import request, redirect
 
 @app.route("/")
 def index():
     test_predictions = test_model_load.get_test_predictions()
     return render_template("index.html", test_predictions=test_predictions)
+
+@app.route("/upload_image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            print(image)
+            return redirect(request.url)
+
+    return render_template("upload_image.html")
