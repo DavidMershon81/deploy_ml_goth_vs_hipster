c42c92d97b335ec17c67425fc557acd0405833eb
= <dmershon@universalhappymaker.com>
2020-12-16 19:29:54 -0800

    added upload progress bar

--- flask/app/static/get_prediction_for_image.js
+++ flask/app/static/get_prediction_for_image.js
@@ -1,48 +1,53 @@
 $(document).ready(function() {
-
-	$("#error_alert").hide();
-
-	$('form').on('submit', function(event) {
-
-		event.preventDefault();
-
-		var formData = new FormData($('form')[0]);
-
-		$.ajax({
-			/*
-			xhr : function() {
-				var xhr = new window.XMLHttpRequest();
-
-				xhr.upload.addEventListener('progress', function(e) {
-
-					if (e.lengthComputable) {
-
-						console.log('Bytes Loaded: ' + e.loaded);
-						console.log('Total Size: ' + e.total);
-						console.log('Percentage Uploaded: ' + (e.loaded / e.total))
-
-						var percent = Math.round((e.loaded / e.total) * 100);
-						$('#progressBar').attr('aria-valuenow', percent).css('width', percent + '%').text(percent + '%');
-					}
-
-				});
-
-				return xhr;
-			},*/
-			type : 'POST',
-			url : '/',
-			data : formData,
-			processData : false,
-			contentType : false,
-			success : on_recieve_prediction
-		});
-
-	});
-
+	reset_ui();
+	$('form').on('submit', send_image_to_server);
 });
 
-function on_recieve_prediction(data)
-{   
+function reset_ui() 
+{  
+	$("#prediction_container").html("");
+	$("#progressbar_group").hide();
+	$("#error_alert").hide();
+}
+
+function send_image_to_server(event) {
+	event.preventDefault();
+	var formData = new FormData($('form')[0]);
+
+	$.ajax({
+		xhr : start_tracking_upload_progress,
+		type : 'POST',
+		url : '/',
+		data : formData,
+		processData : false,
+		contentType : false,
+		success : on_recieve_prediction
+	});
+}
+
+function start_tracking_upload_progress() {
+	$("#progressbar_group").show();
+	set_progress_bar_value(0);
+	$("#progress_bar_label").text("Uploading...");
+	var xhr = new window.XMLHttpRequest();
+	xhr.upload.addEventListener('progress', on_upload_progress_update);
+	return xhr;
+}
+
+function on_upload_progress_update(e) {
+	if (e.lengthComputable) {
+		var percent = Math.round((e.loaded / e.total) * 100);
+		set_progress_bar_value(percent);
+	}
+}
+
+function set_progress_bar_value(percent) {  
+	$('#progressBar').attr('aria-valuenow', percent).css('width', percent + '%').text(percent + '%');
+}
+
+function on_recieve_prediction(data) { 
+	$("#progress_bar_label").text("Image Uploaded!");
+
     if(data.error) {
         $("#error_alert").text(data.error).show();
         $("#prediction_container").html("");
